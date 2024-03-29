eaf656c65da3e1452ab000b13b16db3e2e486cea
Justice Arthur <justicea83@gmail.com>
2024-03-29 14:23:12 -0400

    complete endpoint

--- backend/app/chat/serializers.py
+++ backend/app/chat/serializers.py
@@ -1,16 +1,28 @@
-from operator import itemgetter
-
 from googletrans import Translator
-from langchain_core.documents import Document
 from rest_framework import serializers
 from langchain_exa import ExaSearchRetriever
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langdetect import detect
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.runnables import RunnableParallel
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+from chat.utils import retrieve_cached_text
+
+
+class PromptResponse:
+    def __init__(self, output, query):
+        if isinstance(output, str):
+            self.output = self._chunkify(output, chunk_size=1024)
+        else:
+            self.output = output
+        self.query = query
+
+    @staticmethod
+    def _chunkify(text, chunk_size):
+        """Split text into chunks of specified size."""
+        return (text[i:i + chunk_size] for i in range(0, len(text), chunk_size))
 
 
 class ChatSerializer(serializers.Serializer):
@@ -20,6 +32,17 @@ class ChatSerializer(serializers.Serializer):
         # this query will always have a value because of the validation
         query = self.validated_data.pop('query', '')
 
+        original_query = query
+
+        res = retrieve_cached_text(query)
+
+        if res is not None:
+            return PromptResponse(
+                output=res,
+                query=query
+            )
+
+        # check if this has been queried before
         lang = detect(query)
 
         # we can validate to work with only French and English,
@@ -30,11 +53,7 @@ class ChatSerializer(serializers.Serializer):
         if lang != 'en':
             query = self.translate(query)
 
-        print('\n\n QUERY')
-        # this is the english text
-        print(query)
-
-        return self.search_humber(query, lang)
+        return self.search_humber(original_query, query, lang)
 
     @staticmethod
     def translate(query: str, dest_language: str = 'en') -> str:
@@ -43,7 +62,7 @@ class ChatSerializer(serializers.Serializer):
         return translation.text
 
     @staticmethod
-    def search_humber(query: str, lang: str):
+    def search_humber(original_query: str, query: str, lang: str):
         retriever = ExaSearchRetriever(k=3, include_domains=['https://humber.ca'], highlights=True)
 
         document_prompt = PromptTemplate.from_template("""
@@ -82,7 +101,10 @@ class ChatSerializer(serializers.Serializer):
             ),
         ])
 
-        llm = ChatOpenAI()
+        llm = ChatOpenAI(
+            stream=True
+        )
+
         parser = StrOutputParser()
 
         chain = RunnableParallel(
@@ -91,7 +113,12 @@ class ChatSerializer(serializers.Serializer):
             lang=lambda x: x["lang"]
         ) | generation_prompt | llm | parser
 
-        return chain.invoke({
+        output = chain.stream({
             "query": query,
             "lang": lang
         })
+
+        return PromptResponse(
+            output=output,
+            query=original_query
+        )
