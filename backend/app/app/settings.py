97291d4fa74a3e6d81c3c2f33a4a549d94f7bb44
Justice Arthur <justicea83@gmail.com>
2024-04-02 12:07:18 -0400

    cjange position of cors

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -162,6 +162,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://40.90.252.117:2001",
 ]
 
